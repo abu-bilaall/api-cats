6fede1de002b4a0c3a1f67a26170b665fc512197
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-02-18 15:54:55 +0100

    Wrap API logic in a function and add a summon btn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const img = document.querySelector('img');
+const img = document.querySelector("img");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=WSykRl2DGkakEq1NstuZyd2AY7kJv7jz&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+function giphyCats() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=WSykRl2DGkakEq1NstuZyd2AY7kJv7jz&s=cats",
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+giphyCats();
+
+const summon = document.querySelector("button");
+summon.addEventListener('click', () => {
+    giphyCats();
+});
