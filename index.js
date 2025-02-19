2a1863209babca254d3c489ae5d542c09f8ff597
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-02-19 16:16:05 +0100

    Refractor giphyCats and add summonInput logic

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const img = document.querySelector("img");
 
-function giphyCats() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=WSykRl2DGkakEq1NstuZyd2AY7kJv7jz&s=cats",
+function summonCat(input='cats') {
+  let catRequest = new Request(
+    `https://api.giphy.com/v1/gifs/translate?api_key=WSykRl2DGkakEq1NstuZyd2AY7kJv7jz&s=${input}`,
     { mode: "cors" }
-  )
+  );
+
+  fetch(catRequest)
     .then(function (response) {
+      if(!response.ok) throw new Error(`Response status: ${response.status}`);
       return response.json();
     })
     .then(function (response) {
+      // if(Array.isArray(response)  && response.length === 0) console.log('Cat said: "Meow you! I am not coming."');
       img.src = response.data.images.original.url;
-    });
+    })
+    .catch((error) => console.error(error));
 }
 
-giphyCats();
+summonCat();
 
 const summon = document.querySelector("button");
-summon.addEventListener('click', () => {
-    giphyCats();
+summon.addEventListener("click", () => {
+  summonCat();
+});
+
+const summonInput = document.querySelector("input");
+summonInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    summonCat(summonInput.value);
+    summonInput.value = "";
+  }
 });
