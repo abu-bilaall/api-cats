efc8050ab247874e3bb41c2de24f39395574bce2
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2025-06-23 00:41:23 +0100

    feat: refactor to use async/await

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const img = document.querySelector("img");
 const searchError = document.querySelector("p.error");
 
-function summonCat(input = "cats") {
-  let catRequest = new Request(
-    `https://api.giphy.com/v1/gifs/translate?api_key=WSykRl2DGkakEq1NstuZyd2AY7kJv7jz&s=${input}`,
-    { mode: "cors" }
-  );
+async function summonCat(input = "cats") {
+  try {
+    let catRequest = new Request(
+      `https://api.giphy.com/v1/gifs/translate?api_key=WSykRl2DGkakEq1NstuZyd2AY7kJv7jz&s=${input}`,
+      { mode: "cors" }
+    );
 
-  fetch(catRequest)
-    .then(function (response) {
-      if (!response.ok) throw new Error(`Response status: ${response.status}`);
-      return response.json();
-    })
-    .then(function (response) {
-      if (Array.isArray(response.data) && response.data.length === 0) {
-        img.src = "./cat-not-found.png";
-      } else {
-        img.src = response.data.images.original.url;
-      }
-    })
-    .catch((error) => {
-      searchError.textContent = error;
-    });
+    const response = await fetch(catRequest);
+    if (!response.ok) throw new Error(`Response status: ${response.status}`);
+    const catData = await response.json();
+    if (Array.isArray(catData.data) && catData.data.length === 0) {
+      img.src = "./cat-not-found.png";
+    } else {
+      img.src = catData.data.images.original.url;
+    }
+  } catch (error) {
+    searchError.textContent = error;
+  }
 }
 
 summonCat();
